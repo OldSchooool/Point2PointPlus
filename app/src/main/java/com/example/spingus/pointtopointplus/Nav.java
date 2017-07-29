8714abfe25720ee87e34b02d752da9157e6af834
Unknown <responsiblecheesecake@gmail.com>
2017-07-29 18:30:16 +1000

    Fixed?

--- app/src/main/java/com/example/spingus/pointtopointplus/Nav.java
+++ app/src/main/java/com/example/spingus/pointtopointplus/Nav.java
@@ -182,7 +182,6 @@ public class Nav extends AppCompatActivity
             e.printStackTrace();
         }
 
-        Log.d("hi",);
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
